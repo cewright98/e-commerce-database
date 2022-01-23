a81b9857910996b2ea4002b8839e362c7dcc5f89
Claire <cewright1998@gmail.com>
2022-01-23 12:27:36 -0500

    debugging ENOENT error

--- config/connection.js
+++ config/connection.js
@@ -8,8 +8,7 @@ const sequelize = process.env.JAWSDB_URL
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
-        decimalNumbers: true,
-        socketPath: '/tmp/mysql.sock'
+        decimalNumbers: true
       },
     });
 
